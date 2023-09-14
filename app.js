765e33fc1052754956d29a07eeccaf65277a2ba4
belaSimoes <122486885+belaSimoes@users.noreply.github.com>
2023-09-14 14:26:15 -0400

    bebela

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+
 app.get('/', (req, res) => {
-  res.render('index', { message: 'Olá, Mundo!' });
+  res.render('index');
+});
+
+
+app.get('/produto', (req, res) => {
+  
+  res.render('produto');
 });
 
 app.listen(port, () => {
