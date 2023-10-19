3803f9b7b63cbf47f0241c796e431f3cebd72799
belaSimoes <isabelasimoes205@gmail.com>
2023-10-19 14:22:47 -0400

    s

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-
 app.get('/produto', (req, res) => {
-  
-  res.render('produto');
+  const produto = {
+    productName: 'Pulp Fiction',
+    imagePath: '/imagem_produto1.png',
+    productDescription: 'Assassino que trabalha para a máfia se apaixona pela esposa de seu chefe quando é convidado a acompanhá-la, um boxeador descumpre sua promessa de perder uma luta e um casal tenta um assalto que rapidamente sai do controle.',
+    productPrice: 'R$ 79,99',
+  };
+
+  res.render('produto', { produto });
 });
 
 app.listen(port, () => {
